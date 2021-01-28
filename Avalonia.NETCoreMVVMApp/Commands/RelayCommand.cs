e969f3b67ea8eba7848b735f471e78c1243f4fa5
Ripal Barot <ripplejb@hotmail.com>
2021-01-27 22:26:23 -0600

    Initial Commit

---
+++ Avalonia.NETCoreMVVMApp/Commands/RelayCommand.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Windows.Input;
+
+namespace Avalonia.NETCoreMVVMApp.Commands
+{
+    public class RelayCommand: ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
---
+++ Avalonia.NETCoreMVVMApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Avalonia.NETCoreMVVMApp.Commands;
+using ReactiveUI;
+
+namespace Avalonia.NETCoreMVVMApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _greeting;
+        public MainWindowViewModel()
+        {
+            OnClickMe = new RelayCommand(OnExecute, o => true);
+            _greeting = "Welcome to Avalonia!";
+        }
+
+        public ICommand OnClickMe { get; set; }
+        public string Greeting { get => _greeting; set => this.RaiseAndSetIfChanged(ref _greeting, value); }
+
+        private void OnExecute(object? parameter)
+        {
+            Greeting = Greeting == "Welcome to Avalonia!" ? "You just clicked the button! Click to go back." : "Welcome to Avalonia!";
+        }
+    }
+}
